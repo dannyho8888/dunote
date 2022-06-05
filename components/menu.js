d4ada48c0cd3b3e671dbb4b5f5fc1bfeb9aa02c4
Gkotta <cyj3288503237@gmail.com>
2022-06-05 18:52:10 +0900

    maked the size dynamic

---
+++ components/menu.js
@@ -0,0 +1,13 @@
+class Menu extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        
+      `;
+    }
+  }
+  
+  customElements.define('header-component', Header);
