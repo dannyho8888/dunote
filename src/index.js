c79d560662bb5d24ebbc82a19aadc2cd4f85fc87
Gkotta <cyj3288503237@gmail.com>
2022-06-06 16:46:30 +0900

    localStrage push & new note & render title

--- src/index.js
+++ src/index.js
@@ -1,31 +1,55 @@
-const titleEl = document.getElementById("title-el")
-const testEl = document.getElementById("test-id")
-console.log(titleEl);
+let data = ['terminal command', 'data struc\nTree Map Array']
 
-const data = ['terminal command', 'data struc\nTree Map Array']
+const titleEl = document.getElementById("title-el")
+const textareaEl = document.getElementById("textarea-el")
+const dataLocal = JSON.parse( localStorage.getItem("data") )
+
+renderTitle()
+
+if (dataLocal) {
+    data = dataLocal
+    render(data)
+}
+
+const pushData = () => {
+    data.push(document.getElementById("text-field").value)
+    document.getElementById("text-field").value = "";
+    localStorage.setItem("data", JSON.stringify(data));
+
+}
 
 
 const deleteData = () => {
     
-};
+}
 
 const renderArticle = () => {
     
 };
-let titles = "";
 
-function renderTitle() {
-    console.log('hi')
-    for (let i = 0; i < data.length; i++) {
-        console.log(data[0])
-        titles += `
-            <div id=""class="mt-1 pl-1 flex items-center border-b-2 py-2 hover:bg-gray-300 rounded">
-                <i class="bi bi-bookmark-check-fill text-green-700 pr-1"></i> 
-                ${data[0]}
-            </div>    
+function newNote() {
+    titleEl.innerHTML += `
+            <div  class="mt-1 pl-1 flex items-center border-b-2 py-2 hover:bg-gray-300 rounded">
+                <i class="bi bi-bookmark-check-fill text-green-700 pr-1"></i> new note
+            </div>
         `
-    }
-    titleEl.innerHTML = titles;
 };
 
-
+let titles
+function renderTitle(){
+    titleEl.innerHTML = ``
+    for (let i = 0; i < dataLocal.length; i++) {
+        let title = ""
+        for(let c of dataLocal[i]){
+            if(c=='\n')break
+            title += c
+        }
+        if(title.length>10)
+            title = title.slice(0,10) + "..."
+        titleEl.innerHTML += `
+            <div  class="mt-1 pl-1 flex items-center border-b-2 py-2 hover:bg-gray-300 rounded">
+                <i class="bi bi-bookmark-check-fill text-green-700 pr-1"></i> ${title}
+            </div>
+            `
+    }
+}
